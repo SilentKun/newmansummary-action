5664ad9e6834ea4bf696769c41784db8c17821bf
Igor Tomilin <igor.tomilin.itp@gmail.com>
2021-08-28 01:18:37 +0300

    jhj

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const newman = require('newman');
 
 try {
     newman.run({
-        collection: require('https://api.getpostman.com/collections/15577376-13df1f74-81bd-4c80-a23a-aba31f0016a7?apikey=PMAK-6127edbcffa99200347d31a0-e6ff17b0e53da0faf885390df4edc9da78'), //Add Postman collection path here
-        environment: require('https://api.getpostman.com/environments/15577376-572498e7-3163-4eed-9b8d-191fbbad90f0?apikey=PMAK-6127edbcffa99200347d31a0-e6ff17b0e53da0faf885390df4edc9da78'), //Add postman enviroment file path here 
+        collection: 'https://api.getpostman.com/collections/15577376-13df1f74-81bd-4c80-a23a-aba31f0016a7?apikey=PMAK-6127edbcffa99200347d31a0-e6ff17b0e53da0faf885390df4edc9da78', //Add Postman collection path here
+        environment: 'https://api.getpostman.com/environments/15577376-572498e7-3163-4eed-9b8d-191fbbad90f0?apikey=PMAK-6127edbcffa99200347d31a0-e6ff17b0e53da0faf885390df4edc9da78', //Add postman enviroment file path here 
         reporters:  ['htmlextra'],
         reporter: {
           htmlextra: {
