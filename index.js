3094ae62a56b363daedee8c8eeb664ec0eaf005f
Igor Tomilin <igor.tomilin.itp@gmail.com>
2021-08-28 01:17:32 +0300

    jhgjjh

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { setOutput, setFailed } from '@actions/core';
-import { run } from 'newman';
+const core = require('@actions/core');
+const newman = require('newman');
 
 try {
-    run({
+    newman.run({
         collection: require('https://api.getpostman.com/collections/15577376-13df1f74-81bd-4c80-a23a-aba31f0016a7?apikey=PMAK-6127edbcffa99200347d31a0-e6ff17b0e53da0faf885390df4edc9da78'), //Add Postman collection path here
         environment: require('https://api.getpostman.com/environments/15577376-572498e7-3163-4eed-9b8d-191fbbad90f0?apikey=PMAK-6127edbcffa99200347d31a0-e6ff17b0e53da0faf885390df4edc9da78'), //Add postman enviroment file path here 
         reporters:  ['htmlextra'],
@@ -15,8 +15,8 @@ try {
       .on('start', (err, args) => {
         console.log('running a collection...');
       }).on('done', (err, summary) => {
-        setOutput("summary", `Total: ${summary.run.stats.assertions.total} Failed: ${summary.run.stats.assertions.failed}`);
+        core.setOutput("summary", `Total: ${summary.run.stats.assertions.total} Failed: ${summary.run.stats.assertions.failed}`);
       });
 } catch (error) {
-  setFailed(error.message);
+  core.setFailed(error.message);
 }
