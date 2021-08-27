e119ad56dd83523ec6315c1ddd66e973fa3637d5
Igor Tomilin <igor.tomilin.itp@gmail.com>
2021-08-28 01:43:58 +0300

    changes

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const newman = require('newman');
 
 try {
     newman.run({
-        collection: 'https://api.getpostman.com/collections/15577376-13df1f74-81bd-4c80-a23a-aba31f0016a7?apikey=PMAK-6127edbcffa99200347d31a0-e6ff17b0e53da0faf885390df4edc9da78', //Add Postman collection path here
-        environment: 'https://api.getpostman.com/environments/15577376-572498e7-3163-4eed-9b8d-191fbbad90f0?apikey=PMAK-6127edbcffa99200347d31a0-e6ff17b0e53da0faf885390df4edc9da78', //Add postman enviroment file path here 
+        collection: 'https://api.getpostman.com/collections/15577376-13df1f74-81bd-4c80-a23a-aba31f0016a7?apikey=PMAK-6127edbcffa99200347d31a0-e6ff17b0e53da0faf885390df4edc9da78',
+        environment: 'https://api.getpostman.com/environments/15577376-572498e7-3163-4eed-9b8d-191fbbad90f0?apikey=PMAK-6127edbcffa99200347d31a0-e6ff17b0e53da0faf885390df4edc9da78',
         reporters:  ['htmlextra'],
         reporter: {
           htmlextra: {
               export: 'testResults/htmlreport.html',
-              timezone: "Russia/Moscow"
+              timezone: "Europe/Moscow"
           }
         }
       })
